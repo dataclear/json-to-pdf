61b03af48ff2a57e34eeb993c2e243386455bf85
Ben Staples <staplesben@gmail.com>
2023-04-17 19:37:09 +0100

    moved to jsdoc2md for readme generation

--- bin/docs.js
+++ bin/docs.js
@@ -3,6 +3,10 @@
 const jsdoc2md = require('jsdoc-to-markdown');
 const fs = require('fs');
 
-const data = jsdoc2md.renderSync({ files: './dist/lib/es6/**/*.js' });
+const data = jsdoc2md.renderSync({
+  files: './src/**/*.ts',
+  plugin: ['plugins/markdown', 'node_modules/jsdoc-babel'],
+  separators: true
+});
 
 fs.writeFileSync('./README.md', data, {encoding: 'utf8'});
